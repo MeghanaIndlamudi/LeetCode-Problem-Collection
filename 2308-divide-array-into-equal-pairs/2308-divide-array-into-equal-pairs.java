af6a78c7e545ff767def665636b53f28ef3156f1
Indlamudi Meghana <54998820+MeghanaIndlamudi@users.noreply.github.com>
2025-03-16 19:11:32 -0500

    Time: 6 ms (45.22%), Space: 44.9 MB (13.78%) - LeetHub

---
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+        Set<Integer> unpaired = new HashSet<>();
+        for (int num : nums) {
+            if (unpaired.contains(num)) {
+                unpaired.remove(num);
+            } else {
+                unpaired.add(num);
+            }
+        }
+
+        // Return true if all numbers were paired
+        return unpaired.isEmpty();
+    }
+}
