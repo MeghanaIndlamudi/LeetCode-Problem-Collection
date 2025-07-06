ef24b9f9cf366f3fd5f26a2fdb2c97f3494895a0
Indlamudi Meghana <54998820+MeghanaIndlamudi@users.noreply.github.com>
2025-07-06 11:10:03 -0500

    Time: 141 ms (80%), Space: 73.8 MB (79.3%) - LeetHub

---
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.java
@@ -0,0 +1,43 @@
+class FindSumPairs {
+    private int[] nums1;
+    private int[] nums2;
+    private Map<Integer, Integer> counts;
+
+    public FindSumPairs(int[] nums1, int[] nums2) {
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        this.counts = new HashMap<>();
+
+        for (int num : nums2) {
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
+        }
+    }
+    
+    public void add(int index, int val) {
+        int oldValue = nums2[index];
+        nums2[index] += val;
+        int newValue = nums2[index];
+
+        counts.put(oldValue, counts.get(oldValue) - 1);
+        if (counts.get(oldValue) == 0) {
+            counts.remove(oldValue); 
+        }
+
+        counts.put(newValue, counts.getOrDefault(newValue, 0) + 1);
+    }
+    
+    public int count(int tot) {
+        int totalPairs = 0; 
+        for (int i = 0; i < nums1.length; i++) {
+            int target = tot - nums1[i]; 
+            totalPairs += counts.getOrDefault(target, 0); 
+        }
+        return totalPairs; 
+    }
+}
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs obj = new FindSumPairs(nums1, nums2);
+ * obj.add(index,val);
+ * int param_2 = obj.count(tot);
+ */
